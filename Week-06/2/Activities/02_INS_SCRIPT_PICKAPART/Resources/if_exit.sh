c99f7f088a7fb5db3409ccd5fdfe48da111cb60d
Class Instructor <jerryleiarnold@outlook.com>
2021-07-18 15:11:37 -0500

    fix file

--- Week-06/2/Activities/02_INS_SCRIPT_PICKAPART/Resources/if_exit.sh
+++ Week-06/2/Activities/02_INS_SCRIPT_PICKAPART/Resources/if_exit.sh
@@ -18,7 +18,7 @@
 # if [ <condition1> ] && [ <condition2> ]
 # then
 #   <run_this_command>
-# else
+# else  
 #   <run_this_command>
 # fi
 
@@ -34,26 +34,27 @@ x=5
 y=100
 
 # string variables
-str1="this is a string"
-str2="this is different string"
+str1='this is a string'
+str2='this is different string'
 
 # If $x is equal to $y, run the echo command.
 if [ $x = $y ]
-then
+then 
   echo "X is equal to Y!"
 fi
 
 # If x is not equal to y, exit the script
 if [ $x != $y ]
-then
+then 
   echo "X does not equal Y"
 fi
 
 # If str1 is not equal to str2, run the echo command and exit the script.
-if [ "$str1" != "$str2" ]
-then
+if [ $str1 != $str2 ]
+then 
   echo "These strings do not match."
   echo "Exiting this script."
+  exit
 fi
 
 # If x is greater than y, run the echo command - only works for integer values
@@ -64,7 +65,7 @@ fi
 
 # check if x is less than y - only works for integer values
 if [ $x -lt $y ]
-then
+then 
   echo "$x is less than $y!"
 else
   echo "$x is not less than $y!"
@@ -72,7 +73,7 @@ fi
 
 # check if $str1 is equal to 'this string' AND $x is greater than $y
 # only works if x and y are integers
-if [ "$str1" = 'this string' ] && [ $x -gt $y ]
+if [ $str1 = 'this string' ] && [ $x -gt $y ]
 then
   echo "Those strings match and $x is greater than $y!"
 else
@@ -81,7 +82,7 @@ fi
 
 # check if $str1 is equal to str2 OR $x is less than $y
 # only works if x and y are integers
-if [ "$str1" != "$str2" ] || [ $x -lt $y ]
+if [ $str1 != $str2 ] || [ $x -lt $y ]
 then
   echo "Either those strings don't match OR $x is less than $y!"
 else
@@ -91,11 +92,13 @@ fi
 # check for the /etc directory
 if [ -d /etc ]
 then
+  echo "The /etc directory exists!"
+fi
 
 # check for my_cool_folder
 if [ ! -d /my_cool_folder ]
-then
-  echo "my_cool_folder isn't there!"
+then 
+  echo "my_cool_folder isn\'t there!"
 fi
 
 # check for my_file.txt
@@ -106,7 +109,7 @@ fi
 
 # if sysadmin is running this script, then run an echo command
 if [ $USER != 'sysadmin' ]
-then
+then 
   echo "You are not the sysadmin!"
   exit
 fi
