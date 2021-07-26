196f49cc3245b12ddd5f514fea468a54f8d72deb
Class Instructor <jerryleiarnold@outlook.com>
2021-07-25 19:29:14 -0500

    add 7.2

--- 00/Python/1/Activities/04-Evr_VariableDissection/Unsolved/UNSOLVED_Variables-Dissect.py
+++ 00/Python/1/Activities/04-Evr_VariableDissection/Unsolved/UNSOLVED_Variables-Dissect.py
@@ -1,6 +1,7 @@
 # Part 1
 # =====================================
 
+'''
 # Prints: ? 
 variable_one = 10
 print(variable_one)
@@ -22,10 +23,14 @@ print(difference_of_variables)
 division_variable = variable_one / variable_two
 print(division_variable)
 print(type(division_variable))
+division_variable = int(division_variable)
+print(division_variable)
+print(type(division_variable))
 
 # Prints: ?
 multiplication_variable = variable_one * variable_two
 print(multiplication_variable)
+print(type(multiplication_variable))
 
 
 # Part 2
@@ -40,13 +45,13 @@ print(type(variable_three))
 variable_sum = variable_three + 1
 print(variable_sum)
 print(type(variable_sum))
-
+'''
 
 # Part 3
 # =====================================
 
 # Prints: ?
-variable_four = "This is some nifty text!"
+variable_four = 'This is some nifty text!'
 print(variable_four)
 print(type(variable_four))
 
@@ -54,8 +59,9 @@ print(type(variable_four))
 variable_five = "This is also some sweet text!"
 print(variable_five)
 
+space = " "
 # Prints: ?
-joined_vars = variable_four + " " + variable_five
+joined_vars = variable_four + space + variable_five
 print(joined_vars)
 
 # Prints: ?
@@ -70,7 +76,7 @@ print(adding_together)
 
 # Part 4
 # =====================================
-
+my_fav_number = 14
 # Bonus: Why will the below statement not work (if uncommented)
-# will_not_work = "My favorite number is " + 14
-# print(will_not_work)
+will_not_work = "My favorite number is " + str(my_fav_number)
+print(will_not_work)
